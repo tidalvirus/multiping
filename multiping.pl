8db09313ca1cc4b7f65183b6754a6793c06bd7ce
Siraj 'Sid' Rakhada <sid-git@mindless.co.uk>
2015-05-04 20:48:02 +0800

    Reworked code, and reworded usage, after advice from GAE

--- multiping.pl
+++ multiping.pl
@@ -1,15 +1,101 @@
 #!/usr/bin/env perl -w
 
+use strict;
+use warnings;
+use feature 'say';
+
+use Pod::Usage;
+use Getopt::Long;
+use Net::Ping; 	# Calls in Net::Ping::External also, as we use the
+		# 'external' method for ping - otherwise we'd have to
+		# run this script as root, or suid it.
+
+my $RELEASE = '0.1';
+my $INTERVAL = 1;
+my $COUNT = -1; # Default infinite ping loop
+
+parseCommandLineArguments();
+runPing($COUNT,@ARGV) if (@ARGV);
+
+sub parseCommandLineArguments
+{
+	# pod2usage ripped from http://www.perlmonks.org/?node_id=521812
+	my $HELP	= 0;   # Show help overview.
+	my $MANUAL	= 0;   # Show manual
+	my $VERSION	= 0;   # Show version number and exit.
+
+	#  Parse options. - switch to using a hash, thanks GAE.
+	# https://metacpan.org/pod/Getopt::Long#Storing-options-values-in-a-hash
+	GetOptions(
+		"help",		\$HELP,
+		"manual",	\$MANUAL,
+		"version|V",	\$VERSION,
+		"interval|i=i",	\$INTERVAL,
+		"count|c=i",	\$COUNT
+	);
+    
+	pod2usage(1) if $HELP;
+	pod2usage(-verbose => 2 ) if $MANUAL;
+
+	if ( $VERSION )
+	{
+		say "multiping release $RELEASE";
+		exit;
+	}
+}
+
+
+sub runPing
+{
+	my $limit = shift;
+	my @hosts = @_;
+
+	my $infinite = ($limit == -1);
+
+	my $p = Net::Ping->new("external");
+	$p->hires();
+
+	for (my $count = 0; $count < $limit or $infinite; $count++, sleep $INTERVAL) {
+		unless ( $count % 20 ) {
+			say '-'x75;
+			foreach my $host (@hosts) {
+				printf("%16.15s",$host);
+			}
+			say "\n",'-'x75;
+		}
+
+		foreach my $host (@hosts) {
+			my ($ret, $duration, $ip);
+			eval {
+				local $SIG{ALRM} = sub { die "alarm\n" };
+				alarm 1;
+				($ret, $duration, $ip) = $p->ping($host, 1);
+				alarm 0;
+			};
+			alarm 0;
+			if ($@) {
+				die unless $@ eq "alarm\n";
+				printf("%16.15s", "timed out");
+			}
+			else {
+				my $rtt;
+				printf("%16.15s", sprintf("%.2f ms", 1000 * $duration)) if $ret;
+			}
+		}
+		print "\n";
+	}
+}
+
 =head1 NAME
 
 multiping - A simple tool to ping multiple hosts at the same time
 
 =head1 SYNOPSIS
 
-  multiping [options] [hosts]
+  multiping [options] host...
 
   Help Options:
-   [hosts]	Hosts to ping - at least one required
+   hosts	Hosts to ping - at least one required
 
 =cut
 
@@ -70,103 +156,8 @@ other tools, I can recommend fping.
 
 =cut
 
-use strict;
-use Pod::Usage;
-use Getopt::Long;
-use Net::Ping; 	# Calls in Net::Ping::External also, as we use the
-		# 'external' method for ping - otherwise we'd have to
-		# run this script as root, or suid it.
-
-my $RELEASE = '0.1';
-my $INTERVAL = 1;
-my $COUNT = -1; # Default infinite ping loop
-
-#
-# Parse command line arguments.  These override the values from the
-# configuration file.
-#
-parseCommandLineArguments();
-
-=head2 parseCommandLineArguments
-
-  Parse the arguments specified upon the command line.
-
-=cut
-
-sub parseCommandLineArguments
-{
-	# pod2usage ripped from http://www.perlmonks.org/?node_id=521812
-	my $HELP	= 0;   # Show help overview.
-	my $MANUAL	= 0;   # Show manual
-	my $VERSION	= 0;   # Show version number and exit.
-
-	#  Parse options.
-	#
-	GetOptions(
-		"help",		\$HELP,
-		"manual",	\$MANUAL,
-		"version|V",	\$VERSION,
-       		"interval|i=i",	\$INTERVAL,
-		"count|c=i",	\$COUNT
-	);
-    
-	pod2usage(1) if $HELP;
-	pod2usage(-verbose => 2 ) if $MANUAL;
-
-	runPing($COUNT,@ARGV) if(scalar(@ARGV) >0);
-
-	if ( $VERSION )
-	{
-		print "multiping release $RELEASE\n";
-		exit;
-	}
-}
-
 =head2 runPing
 
   Run the main ping process
 
 =cut
-
-sub runPing
-{
-	my $limit = shift;
-	my @hosts = @ARGV;
-	my $infinite = 0;
-	$infinite = 1 if $limit == -1;
-	$limit = 2 if $infinite; # Need to improve the infinite loop thing
-
-	my $p = Net::Ping->new("external");
-	$p->hires();
-
-	for (my $count = 0; $count < $limit; $count++, sleep $INTERVAL) {
-		if($infinite) { $limit++;} # always increase limit for infinity
-		if( !($count % 20) ) {
-			print '-'x75,"\n";
-			foreach my $host (@hosts) {
-				printf("%16.15s",$host);
-			}
-			print "\n",'-'x75,"\n";
-		}
-
-		foreach my $host (@hosts) {
-			my ($ret, $duration, $ip);
-			eval {
-				local $SIG{ALRM} = sub { die "alarm\n" };
-				alarm 1;
-				($ret, $duration, $ip) = $p->ping($host, 1);
-				alarm 0;
-			};
-			alarm 0;
-			if ($@) {
-				die unless $@ eq "alarm\n";
-				printf("%16.15s","timed out");
-			}
-			else {
-				my $rtt;
-				printf("%16.15s",sprintf("%.2f ms", 1000 * $duration)) if $ret;
-			}
-		}
-		print "\n";
-	}
-}
